90e6cd4325b0a6b440584713dab732c91c048699
Choi-Yuchan <cor330@gmail.com>
2023-08-24 12:37:33 +0900

    create a repository

---
+++ src/main/java/withus/ex/WithusBackendApplication.java
@@ -0,0 +1,13 @@
+package withus.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/withus/ex/WithusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package withus.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
