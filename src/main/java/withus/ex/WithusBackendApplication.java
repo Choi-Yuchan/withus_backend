68d247205dce6b976330ab942c01cee8d0b88f1f
DESKTOP-RB9LR74\USER <gmlsid123@naver.com>
2023-09-19 12:00:51 +0900

    프로퍼티설정수정함(테스트완)

--- src/main/java/withus/ex/WithusBackendApplication.java
+++ src/main/java/withus/ex/WithusBackendApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@PropertySource(value= {"classpath:oAuth2.properties"})
 @Configuration
 public class WithusBackendApplication {
 
