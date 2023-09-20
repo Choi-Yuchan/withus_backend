48afda586a0a41b73968ca9f889416acf726312f
DESKTOP-RB9LR74\USER <gmlsid123@naver.com>
2023-09-20 10:39:35 +0900

    소셜로그인 내용 삭제

--- src/main/java/withus/ex/WithusBackendApplication.java
+++ src/main/java/withus/ex/WithusBackendApplication.java
@@ -2,24 +2,13 @@ package withus.ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
-@PropertySource(value= {"classpath:oAuth2.properties"})
 @Configuration
 public class WithusBackendApplication {
 
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-		   
 	
 	
 	public static void main(String[] args) {
